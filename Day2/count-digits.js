02ffb073585fca685bb7a81527f376cba97c175b
Charlene <dorischarlene64@gmail.com>
2026-06-16 20:18:23 +0500

    I have made a new folder

---
+++ Day2/count-digits.js
@@ -0,0 +1,7 @@
+var num=1235;
+var count=0;
+while(num>0){
+    num=Math.floor(num/10);
+    count++;
+}
+console.log(count)
---
+++ Day2/factorial;.js
@@ -0,0 +1,6 @@
+var factorial=1;
+for(var i=5; i>=1; i--)
+{
+    factorial*=i;
+}
+console.log(factorial)
